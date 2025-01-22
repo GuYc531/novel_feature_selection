f41029eb1fbde08cefee0281a99bee89faf043c5
guy <gguy531997@gmail.com>
2025-01-22 17:24:16 +0200

    update structure

--- feature_selection.py
+++ feature_selection.py
@@ -1,226 +1,283 @@
-from typing import Tuple
+from typing import Tuple, List, Any
 
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from scipy.constants import minute
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, log_loss
-from sklearn.model_selection import StratifiedKFold, KFold
+from sklearn.model_selection import StratifiedKFold, KFold, train_test_split, BaseCrossValidator
 from functools import wraps
 
 from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
+from tqdm import tqdm
 
 valid_models = [RandomForestClassifier,
                 DecisionTreeClassifier,
                 XGBClassifier]
 
+__all__ = ['compute_feature_importance_by_percentile', 'plot_feature_importance_across_quantiles']
 
-def check_is_model_fitted(method):
+
+def _check_is_model_fitted(method):
     @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        if not getattr(self.model, 'feature_importances_', False).any():
+    def wrapper(*args, **kwargs):
+        if not hasattr(kwargs['model'], 'feature_importances_'):
             raise ValueError(f"The model must be fitted before calling `{method.__name__}`.")
-        return method(self, *args, **kwargs)
+        return method(*args, **kwargs)
 
     return wrapper
 
 
-def validate_model(method):
+def _validate_model(method):
     @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        if not isinstance(args[0], tuple(valid_models)):
-            raise TypeError(f"The model {args[0]} must be one of {[type(i).__name__ for i in valid_models]}")
-        return method(self, *args, **kwargs)
+    def wrapper(model, *args, **kwargs):
+        if not isinstance(model, tuple(valid_models)):
+            raise TypeError(f"The model {model} must be one of {[type(i).__name__ for i in valid_models]}")
+        return method(model, *args, **kwargs)
 
     return wrapper
 
 
-class FeatureSelectionByImportance:
-    def __init__(self, df: pd.DataFrame,
-                 y: pd.Series,
-                 metric: str,
-                 quantiles_number: int,
-                 model,
-                 cv=None):
-        """
-
-        :param df:
-        :param y:
-        :param metric:
-        :param quantiles_number:
-        :param cv:
-        :param model:
-        """
-        self.df = df
-        self.y = y
-        self.multi_label = True if len(np.unique(y)) > 2 else False
-
-        self.quantiles_number = self.check_for_valid_quantiles_number(quantiles_number)
-
-        self.model = self.check_model_type(model)
-        # self.model_name = str(type(self.model)).split('.')[-1].split("'")[0]
-        self.model_name = type(model).__name__
-        self.metric = metric
-        self.cv = self.set_cross_validation_policy(cv)
-        self.X_axis_labels = []
-        self.mean_trains, self.std_trains, self.mean_tests, self.std_tests = [], [], [], []
-        self.evaluation_parts = ['train', 'test']
-
-    @staticmethod
-    def set_cross_validation_policy(cv):
-
-        if cv is not None and (isinstance(cv, StratifiedKFold) or isinstance(cv, KFold)):
-            return cv
-
-        stratified = True
-        shuffle = True
-        n_splits = 5
-
-        cv = StratifiedKFold(n_splits=n_splits, shuffle=shuffle) if stratified \
-            else KFold(n_splits=n_splits, shuffle=shuffle)
+def _set_cross_validation_policy(cv: BaseCrossValidator) -> BaseCrossValidator:
+    if cv is not None and (isinstance(cv, StratifiedKFold) or isinstance(cv, KFold)):
         return cv
 
-    def compute_quantile_and_rellevant_df(self, importance_df: pd.DataFrame, percent: float) \
-            -> Tuple[float, pd.DataFrame]:
-        """
+    stratified = True
+    shuffle = True
+    n_splits = 5
+    cv = StratifiedKFold(n_splits=n_splits, shuffle=shuffle) if stratified \
+        else KFold(n_splits=n_splits, shuffle=shuffle)
+    print(f"not given cross validation option , selected one is {cv.__name__}")
 
-        :param importance_df:
-        :param percent:
-        :return:
-        """
-        quantile = importance_df['feature_importance'].quantile(q=percent / self.quantiles_number)
-        percentile_features_df = importance_df[importance_df['feature_importance'] >= quantile]
-        percentile_x = self.df[percentile_features_df["feature_names"]]
-
-        return quantile, percentile_x
-
-    def plot_feature_importance_across_quantiles(self, save_figure_in_path=False, path='') -> None:
-        """
-
-        :param save_figure_in_path:
-        :param path:
-        :return:
-        """
-        x_axis = np.arange(self.quantiles_number)
-        fig = plt.figure()
-        plt.bar(x_axis - 0.3, self.mean_trains, 0.3, label='Train', color='white', edgecolor='black',
-                yerr=self.std_trains)
-        plt.bar(x_axis, self.mean_tests, 0.3, label='Test', color='grey', edgecolor='black', yerr=self.std_tests)
-        plt.xticks(x_axis, self.X_axis_labels, fontsize=8)
-        plt.xlabel("Feature number, Feature quantile", fontsize=12)
-        plt.ylabel(self.metric, fontsize=16)
-        plt.title(f"Mean {self.metric} score +/- std for model {self.model_name}")
-        plt.legend()
-        if save_figure_in_path:
-            plt.savefig(f'{path}feature_selection_figure.png')
-        plt.show()
-        plt.close()
-
-    def compute_feature_importance_by_percentile(self) -> None:
-
-        importance_df = self.compute_feature_importance_df()
-        for percent in range(self.quantiles_number):
-            # get percentile and create new df after filtering
-
-            quantile, percentile_x = self.compute_quantile_and_rellevant_df(importance_df, percent)
-            self.model.fit(percentile_x, self.y)
-            print(f" model = {self.model_name}")
-            print(
-                f"""{round(1 - percent / self.quantiles_number, 3)}, --> total columns =  {len(percentile_x.columns)}, over feature importance {round(quantile, 3)}""")
-            self.X_axis_labels.append(f'{len(percentile_x.columns)}, {round(quantile, 2)}')
-
-            # for flag in self.flags:
-            mean, std, features_importances = self.compute_cv_metric_score(percentile_x)
-
-            self.mean_trains.append(mean['train'])
-            self.std_trains.append(std['train'])
-
-            self.mean_tests.append(mean['test'])
-            self.std_tests.append(std['test'])
+    return cv
 
 
-    @check_is_model_fitted
-    def compute_feature_importance_df(self) -> pd.DataFrame:
-        """
-        Compute feature importance for fitted model.
-        :return: pd.DataFrame of feature_names and feature_importance
-        """
-        importance_scores, feature_names = self.model.feature_importances_, self.model.feature_names_in_
-        importance_df = pd.DataFrame({'feature_names': feature_names,
-                                      'feature_importance': importance_scores}).sort_values(
-            by='feature_importance', ascending=True)
+def _compute_quantile_and_relevant_df(df: pd.DataFrame, importance_df: pd.DataFrame, percent: float,
+                                      quantiles_number: int) \
+        -> Tuple[float, pd.DataFrame]:
+    """
 
-        return importance_df
+    :param df:
+    :param importance_df:
+    :param percent:
+    :param quantiles_number:
+    :return:
+    """
 
+    quantile = importance_df['feature_importance'].quantile(q=percent / quantiles_number)
+    percentile_features_df = importance_df[importance_df['feature_importance'] >= quantile]
+    percentile_x = df[percentile_features_df["feature_names"]]
 
-    def compute_prediction_metric(self, y_true: np.array, y_predictions: np.array) -> float:
-        """
-
-        :param y_true:
-        :param y_predictions:
-        :return:
-        """
-
-        average = 'weighted' if self.multi_label else 'binary'
-        if self.metric == 'accuracy':
-            return accuracy_score(y_true=y_true, y_pred=y_predictions)
-        elif self.metric == 'precision':
-            return precision_score(y_true=y_true, y_pred=y_predictions, average=average)
-        elif self.metric == 'recall':
-            return recall_score(y_true=y_true, y_pred=y_predictions, average=average)
-        elif self.metric == 'f1':
-            return f1_score(y_true=y_true, y_pred=y_predictions, average=average)
-        # elif self.metric == 'log_loss':
-        #     return log_loss(y_true=y_true, y_pred=y_predictions)
-        else:
-            raise ValueError(f"Selected metric {self.metric} should one of accuracy, precision, recall, f1")
+    return quantile, percentile_x
 
 
-    def compute_cv_metric_score(self, percentile_x: pd.DataFrame) -> Tuple[
-        dict[str, np.floating], dict[str,np.floating], dict[int, any]]:
-        """
+def plot_feature_importance_across_quantiles(
+        quantiles_number: int,
+        mean_trains: list,
+        std_trains: list,
+        mean_tests: list,
+        std_tests: list,
+        x_axis_labels: list,
+        metric: str,
+        model,
+        save_figure_in_path: bool = False,
+        path: str = ''
+) -> None:
+    """
 
-        :param percentile_x:
-        :return:
-        """
-
-        metric_list_train = []
-        metric_list_test = []
-        features_importances = {}
-
-        for i, (train, test) in enumerate(self.cv.split(percentile_x, self.y)):
-            # evaluation_part = train if flag == 'train' else test
-
-            # fit new df and compute feature  importance
-            self.model.fit(percentile_x.iloc[train], self.y[train])
-            features_importance_df = self.compute_feature_importance_df()
-            features_importances[i] = features_importance_df['feature_importance']
-
-            # TODO: return the mean and std for all the feature importance
-            # compute score
-            for evaluation_part in self.evaluation_parts:
-                evaluation_indexes = train if evaluation_part == 'train' else test
-                predictions = self.model.predict(percentile_x.iloc[evaluation_indexes])
-                metric_score = self.compute_prediction_metric(self.y[evaluation_indexes], predictions)
-                if evaluation_part == 'train':
-                    metric_list_train.append(metric_score)
-                else:
-                    metric_list_test.append(metric_score)
-
-        metric_mean_score = {'train': np.mean(metric_list_train),
-                             'test': np.mean(metric_list_test)}
-        metric_std_score = {'train': np.std(metric_list_train),
-                            'test': np.std(metric_list_test)}
-
-        return metric_mean_score, metric_std_score, features_importances
+    :param save_figure_in_path:
+    :param path:
+    :return:
+    """
+    model_name = _extract_model_name(model=model)
+    x_axis = np.arange(quantiles_number)
+    fig = plt.figure()
+    plt.bar(x_axis - 0.3, mean_trains, 0.3, label='Train', color='white', edgecolor='black',
+            yerr=std_trains)
+    plt.bar(x_axis, mean_tests, 0.3, label='Test', color='grey', edgecolor='black', yerr=std_tests)
+    plt.xticks(x_axis, x_axis_labels, fontsize=8)
+    plt.ylim(np.min([mean_trains, mean_tests]) - 0.1, np.max([mean_trains, mean_tests]) + 0.1)
+    plt.xlabel("Feature number, Feature quantile", fontsize=12)
+    plt.ylabel(metric, fontsize=16)
+    plt.title(f"Mean {metric} score +/- std for model {model_name}")
+    plt.legend()
+    if save_figure_in_path:
+        plt.savefig(f'{path}feature_selection_figure_{model_name}.png')
+    plt.show()
+    plt.close()
 
 
-    @validate_model
-    def check_model_type(self, model):
-        return model
+def _extract_model_name(model) -> str:
+    return type(model).__name__
 
 
-    @staticmethod
-    def check_for_valid_quantiles_number(quantiles_number):
-        return quantiles_number
+@_check_is_model_fitted
+def compute_feature_importance_by_percentile(cv: BaseCrossValidator,
+                                             model,
+                                             df: pd.DataFrame,
+                                             y: pd.Series,
+                                             split_to_validation: bool = False,
+                                             quantiles_number: int = 5,
+                                             evaluation_parts: List[str] = ['train', 'test'],
+                                             metric: str = 'f1'
+                                             ) -> Tuple[
+    list[np.floating], list[np.floating], list[np.floating], list[np.floating], list[np.floating], dict[int, Any]]:
+    model = _check_model_type(model)
+    model_name = _extract_model_name(model=model)
+    importance_df = _compute_feature_importance_df(model=model)
+    cv = _set_cross_validation_policy(cv=cv)
+
+    x_axis_labels, features_importance = list(), dict()
+    mean_trains, std_trains, mean_tests, std_tests = list(), list(), list(), list()
+
+    print(f" model = {model_name}")
+
+    for percent in tqdm(range(quantiles_number), desc="Processing Quantiles"):
+        # get percentile and create new df after filtering
+
+        quantile, percentile_x = _compute_quantile_and_relevant_df(df=df,
+                                                                   importance_df=importance_df,
+                                                                   percent=percent,
+                                                                   quantiles_number=quantiles_number)
+
+        model.fit(percentile_x, y)
+        print(
+            f"""{round(1 - percent / quantiles_number, 3)}, --> total columns =  {len(percentile_x.columns)}, over feature importance {round(quantile, 3)}""")
+
+        x_axis_labels.append(f'{len(percentile_x.columns)}, {round(quantile, 2)}')
+
+        mean, std, features_importance = _compute_cv_metric_score(percentile_x=percentile_x,
+                                                                  y=y,
+                                                                  evaluation_parts=evaluation_parts,
+                                                                  cv=cv,
+                                                                  model=model,
+                                                                  metric=metric,
+                                                                  split_to_validation=split_to_validation)
+
+        mean_trains.append(mean['train'])
+        std_trains.append(std['train'])
+
+        mean_tests.append(mean['test'])
+        std_tests.append(std['test'])
+
+    return x_axis_labels, mean_trains, std_trains, mean_tests, std_tests, features_importance
+
+
+def _compute_feature_importance_df(model) -> pd.DataFrame:
+    """
+    Compute feature importance for fitted model.
+    :return: pd.DataFrame of feature_names and feature_importance
+    """
+    importance_scores, feature_names = model.feature_importances_, model.feature_names_in_
+    importance_df = pd.DataFrame({'feature_names': feature_names,
+                                  'feature_importance': importance_scores}).sort_values(
+        by='feature_importance', ascending=True)
+
+    return importance_df
+
+
+def _compute_prediction_metric(y_true: np.array,
+                               y_predictions: np.array,
+                               multi_label: bool = False,
+                               metric: str = 'f1') -> float:
+    """
+
+    :param y_true:
+    :param y_predictions:
+    :return:
+    """
+
+    average = 'weighted' if multi_label else 'binary'
+    if metric == 'accuracy':
+        return accuracy_score(y_true=y_true, y_pred=y_predictions)
+    elif metric == 'precision':
+        return precision_score(y_true=y_true, y_pred=y_predictions, average=average)
+    elif metric == 'recall':
+        return recall_score(y_true=y_true, y_pred=y_predictions, average=average)
+    elif metric == 'f1':
+        return f1_score(y_true=y_true, y_pred=y_predictions, average=average)
+    else:
+        raise ValueError(f"Selected metric {metric} should one of accuracy, precision, recall, f1")
+
+
+def _compute_cv_metric_score(percentile_x: pd.DataFrame,
+                             y: pd.Series,
+                             evaluation_parts: list,
+                             cv,
+                             model,
+                             split_to_validation: bool = False,
+                             validation_size: float = 0.2,
+                             metric: str = 'f1') -> Tuple[
+    dict[str, np.floating], dict[str, np.floating], dict[int, any]]:
+    """
+
+    :param percentile_x:
+    :return:
+    """
+    multi_label = True if len(np.unique(y)) > 2 else False
+
+    metric_list_train, metric_list_test, features_importance = list(), list(), dict()
+    if split_to_validation:
+        percentile_x, x_validation, y, y_validation = train_test_split(x=percentile_x,
+                                                                       y=y,
+                                                                       test_size=validation_size,
+                                                                       random_state=5,
+                                                                       stratify=y)
+        metric_list_validation = []
+
+    for i, (train, test) in enumerate(cv.split(percentile_x, y)):
+        # fit new df and compute feature  importance
+        model.fit(percentile_x.iloc[train], y[train])
+        features_importance_df = _compute_feature_importance_df(model=model)
+        features_importance[i] = features_importance_df['feature_importance']
+
+        # TODO: return the mean and std for all the feature importance
+
+        # compute score
+        for evaluation_part in evaluation_parts:
+            evaluation_indexes = train if evaluation_part == 'train' else test
+
+            predictions = model.predict(percentile_x.iloc[evaluation_indexes])
+            metric_score = _compute_prediction_metric(y_true=y[evaluation_indexes],
+                                                      y_predictions=predictions,
+                                                      multi_label=multi_label,
+                                                      metric=metric)
+
+            if evaluation_part == 'train':
+                metric_list_train.append(metric_score)
+            else:
+                metric_list_test.append(metric_score)
+
+        if split_to_validation:
+            try:
+                predictions = model.predict(x_validation)
+                metric_score = _compute_prediction_metric(y_validation, predictions)
+                metric_list_validation.append(metric_score)
+            except NameError:
+                print(f" x_validation or y_validation or metric_list_validation are not defined")
+
+    metric_mean_score = {'train': np.mean(metric_list_train),
+                         'test': np.mean(metric_list_test)}
+    metric_std_score = {'train': np.std(metric_list_train),
+                        'test': np.std(metric_list_test)}
+
+    if split_to_validation:
+        try:
+            metric_mean_score = {'validation': np.mean(metric_list_validation)}
+            metric_std_score = {'validation': np.std(metric_list_validation)}
+        except NameError:
+            print(f"metric_list_validation is not defined")
+
+    return metric_mean_score, metric_std_score, features_importance
+
+
+@_validate_model
+def _check_model_type(model):
+    return model
+
+
+# TODO: need to validate if quantile number is not big for the splits
+def _check_for_valid_quantiles_number(quantiles_number):
+    return quantiles_number
--- main.py
+++ main.py
@@ -1,28 +1,18 @@
-# from PIL import Image
-# import statsmodels.api as sm
 import os
 import pickle
-import sys
-from typing import Tuple
 
 import matplotlib.pyplot as plt
-# import imputation
-import numpy as np
 import pandas as pd
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split, StratifiedKFold, KFold
-from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import StratifiedKFold, KFold
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.utils._bunch import Bunch
-from sklearn.utils.validation import check_is_fitted
 from xgboost import XGBClassifier
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, accuracy_score, \
-    roc_curve, roc_auc_score, log_loss, auc, RocCurveDisplay
 import configs
-from feature_selection import FeatureSelectionByImportance
+from feature_selection import compute_feature_importance_by_percentile, plot_feature_importance_across_quantiles, \
+    _compute_feature_importance_df
 
-# os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz/bin/'
 data_dir = r'data/'
 
 
@@ -60,39 +50,27 @@ def describe_dataset(df: pd.DataFrame, y: pd.Series) -> None:
     print(f"describe dataset: \n {df.describe()}")
 
 
-def return_auc_score(test, labels, model):
-    """
-
-    :param test:
-    :param labels:
-    :param model:
-    :return:
-    """
-    pred_test = model.predict_proba(test.values)[:, 1]
-    fpr, tpr, thresholds = roc_curve(labels, pred_test)
-    roc_auc_test = auc(fpr, tpr)
-    preds = model.predict(test)
-
-    return roc_auc_test
+def plot_basic_feature_importance(model) -> None:
+    feature_importance_df = _compute_feature_importance_df(model=model)
+    plt.bar(feature_importance_df['feature_names'], feature_importance_df['feature_importance'])
+    plt.title("Feature Importances")
+    plt.xlabel("Features")
+    plt.ylabel("Importance Score")
+    plt.xticks(rotation=45)
+    plt.show()
 
 
 dataset = read_covtype_dataset(data_dir)
-# df, utils = imputation.return_dataframe()
+# dataset  = datasets.read_
 df = dataset.data
 y = dataset.target
 
-X = df.iloc[:1000]
-y = y[:1000]
+small_db = False
+X = df.iloc[:1000] if small_db else df
+y = y[:1000] if small_db else y
 
 y = pd.Series([i - 1 for i in y])  # change labels from 1 to 7 to 0 to 6
 describe_dataset(df, y)
-
-# split to train test
-# x_train, x_test, y_train, y_test = train_test_split(df,
-#                                                     y,
-#                                                     test_size=0.1,
-#                                                     random_state=5,
-#                                                     stratify=y)
 
 # choose your best hyperparameters based on grid search
 xgboost_model = XGBClassifier(**configs.xgboost_hyperparametes)
@@ -103,13 +81,12 @@ models = [xgboost_model,
           random_forest_model,
           decision_tree_model]
 
-model = random_forest_model
-scores = ['roc-auc']
-quantiles_number = 8
+model = xgboost_model
+
+quantiles_number = 5
 
 # for model in models:
 # first we fit the model with all features to get general feature importance
-model.fit(X, y)
 
 stratified = True
 shuffle = True
@@ -118,11 +95,34 @@ n_splits = 5
 cv = StratifiedKFold(n_splits=n_splits, shuffle=shuffle) if stratified \
     else KFold(n_splits=n_splits, shuffle=shuffle)
 
-# TODO: add check if quantile has features in it
-#TODO: combine all function with xgboost model and desicion tree
-# metric = 'accuracy'
-# recall_score, f1_score
+# TODO: combine all function with xgboost model and desicion tree VVVV
+# TODO: maybe create package which you dont need to create instance and
+#  only return objects hceck with gpt what is better VVV
+# TODO : fix return mean std of feature importacne
+# TODO: chose another data set with high dimentionality
 metric = 'f1'
-feature_class = FeatureSelectionByImportance(X, y, metric, quantiles_number, model, cv )
-feature_class.compute_feature_importance_by_percentile()
-feature_class.plot_feature_importance_across_quantiles(save_figure_in_path=True)
+split_to_validation = False
+
+for model in models:
+    model.fit(X, y)
+    plot_basic_feature_importance(model)
+    x_axis_labels, mean_trains, std_trains, mean_tests, std_tests, features_importance = compute_feature_importance_by_percentile(
+        model=model,
+        cv=cv,
+        df=X,
+        y=y,
+        split_to_validation=split_to_validation,
+        quantiles_number=quantiles_number,
+        metric=metric
+    )
+
+    plot_feature_importance_across_quantiles(
+        mean_trains=mean_trains,
+        std_trains=std_trains,
+        mean_tests=mean_tests,
+        std_tests=std_tests,
+        x_axis_labels=x_axis_labels,
+        quantiles_number=quantiles_number,
+        metric='f1',
+        model=model,
+        save_figure_in_path=True)
