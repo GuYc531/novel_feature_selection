219c4dbcdeedd3ca5d144dd9839e5119e17a7650
guy <gguy531997@gmail.com>
2025-01-28 17:27:52 +0200

    final version

--- main.py
+++ main.py
@@ -12,30 +12,12 @@ from sklearn.model_selection import StratifiedKFold, KFold
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.utils._bunch import Bunch
 from xgboost import XGBClassifier
+
 import configs
 from feature_selection import compute_feature_importance_by_percentile, plot_feature_importance_across_quantiles, \
     _compute_feature_importance_df
 
 data_dir = r'data/'
-
-
-def read_covtype_dataset(data_dir: str) -> Bunch:
-    """
-
-    :param data_dir:
-    :return:
-    """
-    if os.path.isfile(data_dir + 'covtype.pkl'):
-        print("loading dataset from disk")
-        with open(data_dir + 'covtype.pkl', 'rb') as f:
-            dataset = pickle.load(f)
-    else:
-        print("loading dataset from HTTP request")
-        dataset = datasets.fetch_covtype(as_frame=True, random_state=1, shuffle=True)
-        with open(data_dir + 'covtype.pkl', "wb") as file:
-            pickle.dump(dataset, file)
-
-    return dataset
 
 
 def describe_dataset(df: pd.DataFrame, y: pd.Series) -> None:
@@ -62,61 +44,6 @@ def plot_basic_feature_importance(model) -> None:
     plt.ylabel("Importance Score")
     plt.xticks(rotation=45)
     plt.show()
-
-
-def initialize_data_set(small_db=False):
-    dataset = read_covtype_dataset(data_dir)
-    df = dataset.data
-    y = dataset.target
-
-    X = df.iloc[:1000] if small_db else df
-    y = y[:1000] if small_db else y
-
-    y = pd.Series([i - 1 for i in y])  # change labels from 1 to 7 to 0 to 6
-    return X, y
-
-
-def load_secom_data_set() -> Tuple[pd.DataFrame, pd.Series]:
-    # Define the path to the zip file and the directory to extract to
-    zip_file_path = 'data/secom.zip'
-    extract_to_directory = 'data/secom/'
-
-    if not os.path.exists(extract_to_directory):
-        with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-            zip_ref.extractall(extract_to_directory)
-
-        print(f"Extracted files to {extract_to_directory}")
-
-    X = pd.read_csv(f'{data_dir}secom/secom.data', delimiter=' ', header=None)
-    y = pd.read_csv(f'{data_dir}secom/secom_labels.data', delimiter=' ', header=None).iloc[:, 0]
-    y = pd.Series([i if i == 1 else 0 for i in y])
-    return X, y
-
-
-def load_madelon_data_set() -> Tuple[pd.DataFrame, pd.Series]:
-    # Define the path to the zip file and the directory to extract to
-    zip_file_path = 'data/madelon.zip'
-    extract_to_directory = 'data/madelon/'
-
-    if not os.path.exists(extract_to_directory):
-        with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-            zip_ref.extractall(extract_to_directory)
-
-        print(f"Extracted files to {extract_to_directory}")
-
-    X = pd.read_csv(f'{data_dir}madelon/MADELON/madelon_train.data', delimiter=' ', header=None)
-    y = pd.read_csv(f'{data_dir}madelon/MADELON/madelon_train.labels', delimiter=' ', header=None).iloc[:, 0]
-    y = pd.Series([i if i == 1 else 0 for i in y])
-    return X, y
-
-
-def fix_df_to_pipeline(X: pd.DataFrame) -> pd.DataFrame:
-    # fix X column names
-    X.columns = [f"feature_{i}" for i in range(X.shape[1])]
-
-    # Fill NaN values with column means
-    X = X.fillna(X.mean())
-    return X
 
 
 def load_smoking_drinking_dataset():
